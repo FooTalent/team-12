b5582b16ac765a3e84bcbbc523bc7f95a0f28b80
unknown <gastonbenitez97.gb@gmail.com>
2024-07-11 17:49:14 -0300

    primera creacion de rutas

---
+++ frontend/src/componentes/Agenda/Agenda.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Agenda = () => {
+  return (
+    <div>
+      <p>aqui va la agenda</p>
+    </div>
+  )
+}
+
+export default Agenda
---
+++ frontend/src/componentes/IniciarSesion/IniciarSesion.jsx
@@ -0,0 +1,10 @@
+
+const IniciarSesion = () => {
+  return (
+    <div>
+      <p>iniciar sesion aqui</p>
+    </div>
+  )
+}
+
+export default IniciarSesion
