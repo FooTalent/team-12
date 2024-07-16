36d49c838d8708566ffdef395ddf1adfad8f59df
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-07-16 16:07:14 -0500

    fix error name Registro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Agenda from './pages/Daily/Agenda'
 import IniciarSesion from './pages/Login/IniciarSesion'
 import Home from './pages/Home/Home'
-import Registro from './pages/Registro/Registro';
+import Registro from './pages/Register/Registro';
 
 function App() {
 return(
