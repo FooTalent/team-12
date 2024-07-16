0fa4b67325230037757e9a1557e0a44c07c8be88
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-07-16 17:40:18 -0500

    create components (button, input and navbar) + add roboto font in index.css

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import PropTypes from "prop-types";
 
-export default function Button() {
+export default function Button({ children, className, type }) {
   return (
-    <div>Button</div>
-  )
+    <button
+      className={`rounded-[4px] font-semibold px-4 py-2 transition-all ${className}`}
+      type={type}
+    >
+      {children}
+    </button>
+  );
 }
+
+// esto sirve para que el boton sea requerido y no se pueda usar sin el children
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
+};
--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import PropTypes from "prop-types";
 
-export default function Input() {
+export default function Input({ placeholder, className }) {
   return (
-    <div>Input</div>
-  )
+    <input
+      className={`bg-[#e9ecf0] px-2 py-2 mx-2 placeholder-[#2b3849] rounded-[4px] ${className}`}
+      placeholder={`${placeholder}`}
+    ></input>
+  );
 }
+
+Input.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
