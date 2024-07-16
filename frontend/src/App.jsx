3997a89583765068b9177f335b2cad1704692678
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-07-16 15:05:08 -0500

    base structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Agenda from './componentes/Agenda/Agenda'
-import IniciarSesion from './componentes/IniciarSesion/IniciarSesion'
-import Home from './componentes/Home/Home'
-import Registro from './componentes/Registro/Registro';
+import Agenda from './pages/Daily/Agenda'
+import IniciarSesion from './pages/Login/IniciarSesion'
+import Home from './pages/Home/Home'
+import Registro from './pages/Registro/Registro';
+
 function App() {
 return(
  <>
