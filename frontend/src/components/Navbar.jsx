8b2fe2075eb80e0f094ae69c00ccbabe1895a993
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-07-18 01:11:45 -0500

    add logo to navbar + ui for route home + add custom config to tailwind

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,26 @@
+import { Link } from "react-router-dom";
+import Logo from "../assets/LogoDental.svg";
+
 export default function Navbar() {
   return (
     <nav
-      className="p-4"
+      className="py-3"
       style={{
         backgroundImage: "linear-gradient(to bottom, #7ec2f3, #1a54dc)",
       }}
     >
-      <div className="pl-28">
-        <p className="text-white font-roboto">DentPlanner</p>
-        <p className="text-white">0800 5936-4925</p>
+      <div className="pl-[120px]">
+        <div className=" flex items-center">
+          <Link to={"/"} className="flex items-center">
+            <p className="text-white text-4xl font-bold font-nunito mr-2">
+              DentPlanner
+            </p>
+            <img src={Logo} alt="Logo" />
+          </Link>
+        </div>
+        <div className="number">
+          <p className="text-white font-normal text-2xl">0800 5936-4925</p>
+        </div>
       </div>
     </nav>
   );
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
+import CardWelcome from "../../sections/Home/CardWelcome";
+import Navbar from "../../components/Navbar";
+import Profile from "../../sections/Home/Profile";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex flex-col min-h-screen bg-[#fafdff]">
+      <Navbar />
+      <Profile />
+      <CardWelcome />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
