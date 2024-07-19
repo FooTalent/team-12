ba32012374d48e3262ebfeb4a5930e86c0c12ee2
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-07-19 18:28:50 -0500

    fix path Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Agenda from './pages/Daily/Agenda'
-import Login from './pages/Login/Login'
-import Home from './pages/Home/Home'
-import Registro from './pages/Register/Registro';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Agenda from "./pages/Daily/Agenda";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import Registro from "./pages/Register/Registro";
 
 function App() {
-return(
- <>
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/agenda" element={<Agenda />} />
-      <Route path="/iniciarSesion" element={<Login />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/registrar" element={<Registro />} />
-    </Routes>
-  </Router>
-</>
-)
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/inicio" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/registrar" element={<Registro />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
-
