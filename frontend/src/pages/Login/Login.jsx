517ec29e1b7ced24beaa03a855f54c6530ddc4d7
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-07-22 00:17:45 -0500

    add new route historia-clinica + add Navbar to all pages

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,12 +1,9 @@
-
-import Navbar from '../../components/Navbar'
 import LoginSesion from '../../sections/Login/LoginSesion'
 
 const IniciarSesion = () => {
 
   return (
     <>
-    <Navbar/>
     <LoginSesion/>
     </>
   )
