a7a3f7c31c04106885bf0e40c696dbb3394b35bf
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-07-24 20:34:51 -0500

    fix ui + add menuDrow for navbar + ajust boxShadow for tailwind

--- frontend/src/sections/Home/Profile.jsx
+++ frontend/src/sections/Home/Profile.jsx
@@ -1,7 +1,7 @@
 export default function Profile() {
   const perfil = "Secretario";
   return (
-    <div className="flex bg-[#eef3f7] justify-end pr-[120px] py-3">
+    <div className="sm:flex hidden bg-[#eef3f7] w-full justify-end pr-[120px] py-3">
       <p className="text-base text-[#262626]">Perfil: {perfil}</p>
     </div>
   );
