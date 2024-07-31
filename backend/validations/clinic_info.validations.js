976fdcfea13704cba20c4455ac9fa734449226ca
MBareiro <58647378+MBareiro@users.noreply.github.com>
2024-07-30 22:20:16 -0300

    Correccion clinic info validations

--- backend/validations/clinic_info.validations.js
+++ backend/validations/clinic_info.validations.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const clinicInfoSchema = Joi.object({
   name: Joi.string().min(1).max(255),
-  phone_number: Joi.string().required(),
+  phone_number: Joi.string(),
   address: Joi.string().min(1).max(255),
   email: Joi.string().email(),
   opening_hours: Joi.string().pattern(/^([01]\d|2[0-3]):([0-5]\d)$/), // Formato HH:mm
