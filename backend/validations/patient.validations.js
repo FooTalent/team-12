d8cef6bd4425254d5bbe6a5005e670344e30214b
MBareiro <58647378+MBareiro@users.noreply.github.com>
2024-08-01 23:02:42 -0300

    Correccion de validacion patient

--- backend/validations/patient.validations.js
+++ backend/validations/patient.validations.js
@@ -3,10 +3,10 @@ const Joi = require('joi');
 const patientSchema = Joi.object({
   first_name: Joi.string().min(1).max(255).required(),
   last_name: Joi.string().min(1).max(255).required(),
-  birth_date: Joi.date().iso().required(), // Formato ISO para fechas
+  birth_date: Joi.date().iso().required(), 
   dni: Joi.string().alphanum().min(1).max(20).required(),
-  phone_number: Joi.string().required(), // Ejemplo para números de teléfono internacionales
-  alternative_phone_number: Joi.string().pattern(/^\+?[1-9]\d{1,14}$/).optional(),
+  phone_number: Joi.string().required(),
+  alternative_phone_number: Joi.string().optional(), 
   email: Joi.string().email().required(),
 });
 
